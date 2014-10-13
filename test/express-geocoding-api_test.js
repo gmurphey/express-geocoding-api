6346a4a766e0fcc306258ecd03dfa39710ae5cb3
Garrett Murphey <gmurphey@gmurphey.com>
2014-10-13 13:40:25 -0400

    Removing tests.

--- test/express-geocoding-api_test.js
+++ test/express-geocoding-api_test.js
@@ -22,15 +22,3 @@ var lib = require('../lib/express-geocoding-api.js');
     test.ifError(value)
 */
 
-exports['awesome'] = {
-  setUp: function(done) {
-    // setup here
-    done();
-  },
-  'no args': function(test) {
-    test.expect(1);
-    // tests here
-    test.equal(express_geocoding_api.awesome(), 'awesome', 'should be awesome.');
-    test.done();
-  },
-};
